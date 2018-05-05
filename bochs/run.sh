40ca19b6a59fa8bdde9ddc8d08dbb9d8ae286e4a
lambda <2524872+lam8da@users.noreply.github.com>
2018-05-04 17:05:25 -0700

    Fix run.sh for two process example

--- bochs/run.sh
+++ bochs/run.sh
@@ -17,13 +17,21 @@ diff_bin() {
 }
 build_img() {
   local asm=${1:-boot.asm}
+	shift
   local bin=${asm//.asm/.bin}
   nasm "$asm" -f bin -o "$bin"
   # Copy the bin file to img file
-  dd if="$bin" of="$IMG_FILE" bs=512 count=1 conv=notrunc
+  dd if="$bin" of="$IMG_FILE" bs=512 conv=notrunc "$@"
+}
+build_hello_world() {
+  build_img hello_world_boot.asm count=1
+}
+build_two_process() {
+  build_img two_process_boot.asm count=1
+  build_img two_process_head.asm oseek=1
 }
 run() {
-  build_img "$1"
+	# Type 'continue' or 'c' to start the virtual machine.
   bochs -q -f bochsrc.txt
 }
 run_segment_descriptor_parser() {
@@ -31,6 +39,7 @@ run_segment_descriptor_parser() {
 	gcc -o $out segment_descriptor_parser.cc && ./$out
 }
 
-# Type 'continue' or 'c' to start the virtual machine.
-# run hello_world_boot.asm
-run_segment_descriptor_parser
+# build_hello_world
+build_two_process
+run
+# run_segment_descriptor_parser
