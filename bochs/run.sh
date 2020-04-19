8f2b679e4c2d574dcd02bf72c9add45d9890d5ce
lambda <2524872+lam8da@users.noreply.github.com>
2020-04-19 01:20:36 -0700

    Fix indentation

--- bochs/run.sh
+++ bochs/run.sh
@@ -4,16 +4,16 @@ set -x
 IMG_FILE=bin/boot.img
 
 install_tools() {
-	# sudo chown -R $(whoami) /usr/local/bin /usr/local/etc /usr/local/sbin /usr/local/share /usr/local/share/doc
+  # sudo chown -R $(whoami) /usr/local/bin /usr/local/etc /usr/local/sbin /usr/local/share /usr/local/share/doc
   sudo chown -R "$USER":admin /usr/local/share /usr/local/bin
   brew install bochs
   brew install nasm
 }
 diff_bin() {
-	if [[ "$#" -ne 2 ]]; then
-	  echo 'Usage: diff_bin <file1> <file2>'
-		return
-	fi
+  if [[ "$#" -ne 2 ]]; then
+    echo 'Usage: diff_bin <file1> <file2>'
+    return
+  fi
   local o1=/tmp/hexdump-"$1"
   local o2=/tmp/hexdump-"$2"
   xxd "$1" > "$o1"
@@ -21,14 +21,14 @@ diff_bin() {
   diff "$o1" "$o2"
 }
 build_img() {
-	if [[ "$#" -lt 1 ]]; then
-	  echo 'Usage: build_img <src> [<dd args>...]'
-		return
-	fi
+  if [[ "$#" -lt 1 ]]; then
+    echo 'Usage: build_img <src> [<dd args>...]'
+    return
+  fi
   local src="$1"
-	shift
+  shift
   local bin=${src//.asm/.bin}
-	bin=${bin//src\//bin\/}
+  bin=${bin//src\//bin\/}
   nasm "$src" -f bin -o "$bin"
   # Copy the bin file to img file
   dd if="$bin" of="$IMG_FILE" bs=512 conv=notrunc "$@"
@@ -41,18 +41,18 @@ build_two_process() {
   build_img src/two_process_head.asm oseek=1
 }
 run() {
-	unset_x
-	echo "===> Type 'c' to start the virtual machine."
-	echo "===> Note we can use the debugger to debug, try:"
-	echo "     > b 0x7c00"
-	echo "     > u/10"
-	echo "===> We can also use ndisasm command to disassemble the .bin/.img file."
-	reset_x
+  unset_x
+  echo "===> Type 'c' to start the virtual machine."
+  echo "===> Note we can use the debugger to debug, try:"
+  echo "     > b 0x7c00"
+  echo "     > u/10"
+  echo "===> We can also use ndisasm command to disassemble the .bin/.img file."
+  reset_x
   bochs -q -f bochsrc.txt
 }
 run_segment_descriptor_parser() {
-	local name=segment_descriptor_parser
-	gcc -o $name.out $name.cc && ./$name.out
+  local name=segment_descriptor_parser
+  gcc -o $name.out $name.cc && ./$name.out
 }
 
 # build_hello_world
